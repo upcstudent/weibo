198a23709ca804df22033bed5a23a1831c6285d8
upcstudent <upcstudent@163.com>
2019-01-21 11:28:52 +0000

    update database config too

--- app/helpers.php
+++ app/helpers.php
@@ -7,11 +7,11 @@ function get_db_config()
         $url=parse_url(getenv("DATABASE_URL"));
 
         return $db_config=[
-            'connection'=>'pgsql',
-            'host'=>$url["host"],
-            'database'=>substr($url["path"],1),
-            'username'=>$url["user"],
-            'password'=>$url["pass"],
+             'connection' => 'pgsql',
+            'host' => $url["host"],
+            'database'  => substr($url["path"], 1),
+            'username'  => $url["user"],
+            'password'  => $url["pass"],
         ];
     }else {
         return $db_config=[
