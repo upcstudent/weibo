a89379ea6f9139b91d4bcbede7bdcab0dbbc311d
upcstudent <upcstudent@163.com>
2019-01-23 08:39:09 +0000

    查看用户列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,7 +11,7 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth', [
-            'except' => ['show', 'create', 'store']
+            'except' => ['show', 'create', 'store','index']
         ]);
 
         $this->middleware('guest', [
@@ -50,13 +50,13 @@ class UsersController extends Controller
         return redirect()->route('users.show',[$user]);
     }
 
-    function edit(User $user)
+    public function edit(User $user)
     {
         $this->authorize('update',$user);
         return view('users.edit',compact('user'));
     }
 
-    function update(User $user,Request $request)
+    public function update(User $user,Request $request)
     {
         $this->authorize('update',$user);
         $this->validate($request,[
@@ -74,5 +74,12 @@ class UsersController extends Controller
         session()->flash('success', '个人资料更新成功！');
 
         return redirect()->route('users.show',$user->id);
+    }
+
+    public function index()
+    {
+        $users = User::paginate(10);
+        //$users=User::all();
+        return view('users.index',compact('users'));
     }
 }
