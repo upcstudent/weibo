c75601b2ad40c95ce202749a2d8c6bb8d7f0d2b2
upcstudent <upcstudent@163.com>
2019-01-23 08:52:41 +0000

    管理员可删除用户

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -82,4 +82,12 @@ class UsersController extends Controller
         //$users=User::all();
         return view('users.index',compact('users'));
     }
+
+    public function destroy(User $user)
+    {
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '成功删除用户！');
+        return back();
+    }
 }
